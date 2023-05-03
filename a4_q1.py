5d0c74aef535d6e7727b937fc8f72fc1b4a8ad65
Haani <116673121+haanisyed@users.noreply.github.com>
2023-05-02 22:21:24 -0400

    Add files via upload
    Question 1: char_prime will map each letter to a unique prime number by writing a function that takes
    a single character and outputs the unique prime that you assign to that character. [HINT:
    you should hard code the mapping between letters and primes in this function so that
    your map is consistent].
    ● Primeify. A recursive function that computes the product of the character primes for a
    given string. This must be a recursive function! You can use the previous function
    (char_prime). Consider how to build the prime value for BANANA if you already know the
    prime function for NANA
    ● is _anagram. This function takes in two strings, and computes their product of primes by
    ‘primeify’ing each string. If the values are equal, then they must be anagrams.
    In your main function, called a4_q1.py Ask the user for two strings that contain only upper-case
    letters. You should confirm that these are in fact strings, and should check this input. However,
    you may choose to give an error, or clean the code however you wish. There are no
    requirements here other than that you make certain the user input is a valid string of only upper
    case letters with no spaces. Then, call the function is_anagram, and give the user a message
    based on this output

---
+++ a4_q1.py
@@ -0,0 +1,45 @@
+"""
+   CISC-121 2023W
+
+   Name: Haani Syed
+   Student Number: 20331181
+   Email: 21ahs7@queensu.ca
+   Date: 2023-03-15
+
+   I confirm that this assignment solution is my own work and conforms to
+   Queen's standards of Academic Integrity
+"""
+from functions import char_prime, primeify, is_anagram #imports functions to be called in lines below.
+def main():
+    '''
+
+    -------------------------------------------------------
+    A4 Question 1's Main function with user friendly statements and calls to functions based on user input:
+    Entry of 1 means def char_prime(my_char) is called, an uppercase alphabet is entered by user and respective prime returned.
+    Entry of 2 means def primeify(my_string) is called, an uppercase string is entered by user and the corresponding product of prime numbers returned.
+    Entry of 3 means def is_anagram(string1, string2) is called, two uppercase strings entered by user and the return is True if they're anagrams and False if not.
+    -------------------------------------------------------
+    Parameters:
+        None directly for def main function. However there are many parameters for each of the respective functions called in this main function.
+    Returns:
+         No direct return. However 3 command based functions are called which have returns.
+    -------------------------------------------------------
+    '''
+    print('Enter an UPPERCASE alphabet for my_char below ')#user friendly statement
+    my_char = input('Enter an UPPERCASE Alphabet: ')
+    print('The corresponding prime number for the entered alphabet is: ', char_prime(my_char)) #user friendly statement
+    print('Enter a word/string in all uppercase ')
+    my_string = input('Enter an UPPERCASE string: ')
+    print('The corresponding product of primes for the entered string is: ', primeify(my_string))#user friendly statement
+    print('Enter two UPPERCASE strings (String1, String2) to determine whether they are anagrams or not ')#user friendly statement
+    string1 = input('Enter an UPPERCASE string/word for String1: ') #user uppercase string1 input.
+    if not string1.isupper(): #Error check. If user enters non-uppercase. Error statement will be printed.
+         print('Error. The string inputted was NOT all UPPERCASE') #Error statement.
+    string2 = input('Enter a UPPERCASE string/word for String2: ') #user uppercase string2 input.
+    if not string2.isupper(): #Error check. If user enters non-uppercase. Error statement will be printed.
+            print('Error. The string inputted was NOT all UPPERCASE')
+    print('The statement that the two entered UPPERCASE strings are anagrams is: ',is_anagram(string1, string2),'.')
+        # above print statement will inform user whether it is True or False that the 2 entered uppercase strings are anagrams.
+
+main() #call to main to let Q1 run
+
